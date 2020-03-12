5b69f4d6058add5b3a2339a47c38665b69af49df
James Errington <james@maprunner.co.uk>
2020-03-12 01:45:37 +0000

    Updates

---
+++ precache-manifest.b4439caa64b2ef0e1f806e14674c3d43.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657a81483a70b02dcb5fb246f762ba76",
+    "url": "/COMP6214-cw1/index.html"
+  },
+  {
+    "revision": "4561d7483d3d42f18fd7",
+    "url": "/COMP6214-cw1/static/css/main.31b36910.chunk.css"
+  },
+  {
+    "revision": "7d899f8c969df9a18277",
+    "url": "/COMP6214-cw1/static/js/2.338f215c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/COMP6214-cw1/static/js/2.338f215c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4561d7483d3d42f18fd7",
+    "url": "/COMP6214-cw1/static/js/main.e9d320ac.chunk.js"
+  },
+  {
+    "revision": "e483cbda2ffcb8b2a824",
+    "url": "/COMP6214-cw1/static/js/runtime-main.e3bc471d.js"
+  },
+  {
+    "revision": "c2c388c8c30bb5ae6fc6797193f7aedf",
+    "url": "/COMP6214-cw1/static/media/Comp6214Data.c2c388c8.csv"
+  },
+  {
+    "revision": "0521496a8ecd6402250899f80467d0d8",
+    "url": "/COMP6214-cw1/static/media/arrow-circle-up.0521496a.svg"
+  },
+  {
+    "revision": "4b95a81df103518984d21d61fecf8318",
+    "url": "/COMP6214-cw1/static/media/broom.4b95a81d.svg"
+  },
+  {
+    "revision": "371710b09fa1dec3c7e29a4c5f177f16",
+    "url": "/COMP6214-cw1/static/media/chart-bar.371710b0.svg"
+  },
+  {
+    "revision": "e44a278717c146b2d9a6cce3668a9474",
+    "url": "/COMP6214-cw1/static/media/link.e44a2787.svg"
+  }
+]);
